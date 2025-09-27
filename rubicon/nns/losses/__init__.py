71ad1e08304035ded0a5ef394508984c8091e83e
Nahum Maurice <nahum.maurice.nm@gmail.com>
2025-09-27 21:38:52 +0300

    ordering imports

--- rubicon/nns/losses/__init__.py
+++ rubicon/nns/losses/__init__.py
@@ -2,7 +2,7 @@
 
 from rubicon.nns.losses._base import LossFn
 from rubicon.nns.losses.cross_entropy import CrossEntropyLoss
-from rubicon.nns.losses.mse import MSELoss
 from rubicon.nns.losses.kare import KARELoss
+from rubicon.nns.losses.mse import MSELoss
 
 __all__ = ["CrossEntropyLoss", "KARELoss", "LossFn", "MSELoss"]
